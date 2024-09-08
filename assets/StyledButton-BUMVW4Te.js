b5a2454b4d3bfd975a78feace45ffc70e59c47d7
emp <arsenii_rusin@mail.ru>
2024-09-09 04:56:35 +0800

    Updates

---
+++ assets/StyledButton-BUMVW4Te.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-Vln5owk7.js";const r=({callback:t,children:o})=>s.jsx("button",{className:"button",onClick:t,children:o});export{r as S};
--- assets/StyledButton-CKZhUUhH.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-pTCheAQo.js";const r=({callback:t,children:o})=>s.jsx("button",{className:"button",onClick:t,children:o});export{r as S};
