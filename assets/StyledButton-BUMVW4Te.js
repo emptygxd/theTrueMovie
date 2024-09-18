7d0aaa22b588bb2a9fb52b001bcdb58982590a15
emp <arsenii_rusin@mail.ru>
2024-09-18 21:14:45 +0800

    Updates

--- assets/StyledButton-BUMVW4Te.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-Vln5owk7.js";const r=({callback:t,children:o})=>s.jsx("button",{className:"button",onClick:t,children:o});export{r as S};
---
+++ assets/StyledButton-D7Ja3p_W.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-Bls0foRN.js";const r=({callback:t,children:o})=>s.jsx("button",{className:"button",onClick:t,children:o});export{r as S};
